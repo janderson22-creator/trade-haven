d7cc434ef5ff7761bc1dbe30ff824326110ecbed
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-10-18 17:30:40 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        alt="Até 55% de desconto esse mês!"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  );
+}
