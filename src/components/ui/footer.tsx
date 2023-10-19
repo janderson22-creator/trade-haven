b579cac17bafb94ff72c04bffe9cddeb2d3954ea
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-10-19 19:32:14 -0300

    feat: add CategoryItem component

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-      © 2023 Copyright <span className=" font-semibold">FSW Store</span>
+      © 2023 Copyright <span className="font-semibold">FSW Store</span>
     </div>
   );
 };
