e98a71b886777bb811d4c9c6fbffc4a56d42be58
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-10-23 18:17:12 -0300

    feat: ensure product can be added to cart

---
+++ src/components/ui/cart.tsx
@@ -0,0 +1,25 @@
+import { ShoppingCartIcon } from "lucide-react";
+import { Badge } from "./badge";
+import { CartContext } from "@/providers/cart";
+import { useContext } from "react";
+
+const Cart = () => {
+  const { products } = useContext(CartContext);
+  return (
+    <div>
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-1.5 text-base uppercase"
+        variant="outline"
+      >
+        <ShoppingCartIcon size={16} />
+        Catálogo
+      </Badge>
+
+      {products.map((product) => (
+        <h1 key={product.id}>{product.name}</h1>
+      ))}
+    </div>
+  );
+};
+
+export default Cart;
