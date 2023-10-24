967c52a3e4075f2b0edf1686d6377d7b558f78ff
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-10-24 11:28:50 -0300

    feat: ensure product quantity can be modified on cart

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -1,18 +1,19 @@
-import { CartProduct } from "@/providers/cart";
+import { CartContext, CartProduct } from "@/providers/cart";
 import Image from "next/image";
 import { Button } from "./button";
 import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "lucide-react";
+import { useContext } from "react";
 
 interface CartItemProps {
   product: CartProduct;
 }
 
 const CartItem: React.FC<CartItemProps> = ({ product }) => {
+  const { decreaseProductQuantity } = useContext(CartContext);
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-4">
-        {/* PARTE ESQUERDA */}
-
         <div className="flex h-[77px] w-[77px] items-center justify-center rounded-lg bg-accent">
           <Image
             src={product.imageUrls[0]}
@@ -24,8 +25,8 @@ const CartItem: React.FC<CartItemProps> = ({ product }) => {
           />
         </div>
 
-        <div className="flex flex-col max-w-[120px]">
-          <p className=" text-ellipsis overflow-hidden whitespace-nowrap text-xs">
+        <div className="flex max-w-[120px] flex-col">
+          <p className=" overflow-hidden text-ellipsis whitespace-nowrap text-xs">
             {product.name}
           </p>
 
@@ -39,7 +40,12 @@ const CartItem: React.FC<CartItemProps> = ({ product }) => {
           </div>
 
           <div className="mt-1 flex items-center gap-1">
-            <Button size="icon" variant="outline" className="h-8 w-8">
+            <Button
+              size="icon"
+              variant="outline"
+              className="h-8 w-8"
+              onClick={() => decreaseProductQuantity(product.id)}
+            >
               <ArrowLeftIcon size={16} />
             </Button>
 
